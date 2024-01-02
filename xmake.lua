c7d09d88059d2d9cb7013ab46df713d505d6b429
sleeping1943 <clseeping@163.com>
2024-01-02 14:37:09 +0800

    删除无用代码

--- xmake.lua
+++ xmake.lua
@@ -38,36 +38,6 @@ target("gb28181-server")
         "pthread"
     )
 
-target("gb28181-client")
-    set_kind("binary")
-    add_files("src/client/src/**.cpp")
-    set_languages("c++11")
-
-    set_targetdir("./")
-    add_cxxflags("-O0", "-Wall", "-g2", "-ggdb")
-    --add_cflags("-g")
-
-    add_includedirs(
-        "./",
-        "/usr/local/include"
-    )
-
-    add_linkdirs(
-        "/usr/local/lib"
-    )
-
-    add_links(
-        "hv",
-        "osip2", 
-        "osipparser2",
-        "eXosip2",
-        "boost_thread"
-    )
-
-    add_syslinks(
-        "pthread"
-    )
-
 target("test-gb28181")
     set_kind("binary")
     add_files("src/test/*.cpp", "src/utils/*.cpp")
