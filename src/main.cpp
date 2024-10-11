38dcfdf3ce1c058777d84cbf4ad33eb12f56116f
sleeping1943 <csleeping@163.com>
2024-10-11 10:56:20 +0800

    使用信号量即时退出主线程循环

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,31 @@
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
+#include <boost/thread/thread_time.hpp>
 #include <chrono>
-#include <iostream>
-#include <ratio>
 #include <thread>
 #include "server.h"
 #include "utils/helper.h"
 #include <signal.h>
 #include "utils/log.h"
-#include "utils/md5.h"
 #include "http/http_server.h"
 #include "xzm_defines.h"
 #include "deleter.h"
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 const static std::string kConfPath = "./conf/config.json";
+
+boost::interprocess::interprocess_semaphore semaphore(0);
 
 void quit_server(int)
 {
     Xzm::Server::is_server_quit.store(true);
     CLOGI(RED, "ready to quit server!!");
+    semaphore.post();
 }
 
 int main(int argc, char** argv)
 {
-    setlocale(LC_CTYPE, "");    // 使ansi编码有效,用于utf8和ansi转码,但是所有lib都回收影响,有风险
+    setlocale(LC_CTYPE, "");    // 使ansi编码有效,用于utf8和ansi转码,但是所有lib都会受影响,有风险
     signal(SIGINT, quit_server);
     Xzm::Server::instance()->Test();
     std::string content;
@@ -55,10 +59,20 @@ int main(int argc, char** argv)
     // 启动删除文件线程
     Xzm::XDeleter::instance()->Start();
     // 等待服务退出
-    unsigned int interval = 30;
+    //unsigned int interval = 30;
+    try {
+    unsigned int interval = 5;
+    boost::system_time wait_time = boost::get_system_time() + boost::posix_time::milliseconds(interval * 1000);
     while (!Xzm::Server::is_server_quit && !Xzm::Server::is_client_all_quit) {
         CLOGI(YELLOW, "wait for server quit[%ds interval]...", interval);
-        std::this_thread::sleep_for(std::chrono::milliseconds(interval * 1000));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(interval * 1000));
+        semaphore.timed_wait(wait_time);
+        wait_time = boost::get_system_time() + boost::posix_time::milliseconds(interval * 1000);
+    }
+    } catch (std::exception& e) {
+        std::stringstream ss;
+        ss << "there is a error:" << e.what();
+        LOGE("%s", ss.str().c_str());
     }
     // 关闭删除文件线程
     Xzm::XDeleter::instance()->Stop();
